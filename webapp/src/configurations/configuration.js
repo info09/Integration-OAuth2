879d3eb92a9769f16cace4dfc65eda7e2495ac16
huytq <huytq3103@gmail.com>
2024-11-09 10:32:46 +0700

    Init project webapp

---
+++ webapp/src/configurations/configuration.js
@@ -0,0 +1,6 @@
+export const OAuthConfig = {
+  clientId:
+    "713242712185-raq25qnib3c2rju9ak6bsvfqb6eve7og.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
