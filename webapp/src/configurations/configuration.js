59fb8dd61e35bcff42497bfbb6de523a63aad023
Quang Huy Trần <37743725+info09@users.noreply.github.com>
2025-06-23 10:12:46 +0700

    commit

--- webapp/src/configurations/configuration.js
+++
@@ -1,6 +0,0 @@
-export const OAuthConfig = {
-  clientId:
-    "713242712185-raq25qnib3c2rju9ak6bsvfqb6eve7og.apps.googleusercontent.com",
-  redirectUri: "http://localhost:3000/authenticate",
-  authUri: "https://accounts.google.com/o/oauth2/auth",
-};
